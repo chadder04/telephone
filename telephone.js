fb54562445c2c6bcf6f93ca2d51d297798855bdc
Chad Garrett <chadgarrett_@hotmail.com>
2017-09-06 01:47:58 +0000

    Initial commit

---
+++ telephone.js
@@ -0,0 +1,25 @@
+/*
+
+  Each number on the telephone dial (except 0 and 1) corresponds to three alphabetic characters. Those correspondences are:
+    2 ABC
+    3 DEF
+    4 GHI
+    5 JKL
+    6 MNO
+    7 PRS
+    8 TUV
+    9 WXY
+
+  Given a seven digit telephone number, print all 2187 possible "words" that number spells.
+  (They may not be real english words, but just some sequence of characters).
+  Since the digits 0 and 1 have no alphabetic equivalent, an input number which contains those digits should be rejected.
+  The input will be one or more seven digit integers from standard input.
+
+*/
+
+function telephone(inputArray) {
+  
+}
+
+//console.log(telephone(process.argv.slice(2)));
+console.log(telephone(8675309));
